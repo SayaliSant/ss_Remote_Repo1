8b07c3cd70b43a07fd4d904d408d660047fc1ef6
Dell <Dell@DESKTOP-QTNC39G>
2023-11-29 20:37:54 +0530

    Infinite loop code

---
+++ ss_Remote_Repo1/InfiniteLoop.java
@@ -0,0 +1,26 @@
+package ss_Remote_Repo1;
+
+
+
+public class InfiniteLoop {
+	public static void main(String[] args) {
+		
+		do {}
+		while(true);
+		
+		for (;;)
+		{
+			
+		}
+		
+		while(true)
+		{
+			
+		}
+	
+		
+		
+		
+	}
+		
+}
