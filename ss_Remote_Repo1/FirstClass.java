e8edc9f546a50ee57c723462ae63baa13031a28d
Dell <Dell@DESKTOP-QTNC39G>
2023-11-30 20:21:08 +0530

    First Class in Java

---
+++ ss_Remote_Repo1/FirstClass.java
@@ -0,0 +1,74 @@
+package ss_Remote_Repo1;
+
+public class FirstClass {
+	public static void main(String[] args) {
+	
+	}
+}
+
+class Human extends FirstClass{
+	String name;
+	int age;
+	String occupation;
+	char sex;
+	String nationality;
+	
+	void canSpeak() {
+		
+	}
+	
+	void canWork() {
+		
+	}
+}
+
+class Laptop extends FirstClass{
+	String brand;
+	String memory;
+	String colour;
+	int prize;
+	String os;
+	
+	void isWorking() {
+		
+	}
+	
+	void isRestartRequird() {
+		
+	}
+	
+}
+
+class SportMan extends FirstClass{
+	String sportName;
+	String counrty;
+	int age;
+	int records;
+	String awards;
+	boolean retired;
+	
+	void sportMatch() {
+		
+	}
+	
+	void isRetired() {
+		
+	}
+	
+}
+
+class Testcase extends FirstClass{
+	int testcaseNumber;
+	String scenario;
+	String actualResult;
+	String expectedResult;
+	boolean result;
+	
+	void isPassFail() {
+		
+	}
+	
+	void isExists() {
+		
+	}
+}
